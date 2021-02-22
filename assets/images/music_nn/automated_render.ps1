02a601128bba5e6b4fadf0c6bb9a6bfa1c835d1c
onecloudemoji <46857300+onecloudemoji@users.noreply.github.com>
2021-02-22 19:24:05 +0930

    Update automated_render.ps1

--- assets/images/music_nn/automated_render.ps1
+++ assets/images/music_nn/automated_render.ps1
@@ -1,28 +1,35 @@
 #1) ask for inputs; how many, length, etc
 $num_outputs = Read-Host -Prompt 'How many tracks to generate?'
 $num_steps = Read-Host -Prompt 'How many steps should they be?'
- 
- 
+
+
 #2) run nn generation and put them into a folder labeled not processed
-polyphony_rnn_generate --run_dir="DIR_WHERE_TRAINED_MODEL_IS_GOING" --hparams="batch_size=64,rnn_layer_sizes=[128,128,128]" --output_dir="DIR_WHERE_GENERATED_FILES_ARE_GOING" --num_outputs=$num_outputs --num_steps=$num_steps --primer_melody="[20]" --condition_on_primer=true --inject_primer_during_generation=false
- 
-#3) get names of things in the not processed folder
- 
+polyphony_rnn_generate --run_dir="C:\Users\root\Desktop\blog posts\nn project\polyphony_rnn\logdir\run3" --hparams="batch_size=64,rnn_layer_sizes=[128,128,128]" --output_dir="E:\SNES MIDIS\Generated Midi" --num_outputs=$num_outputs --num_steps=$num_steps --primer_melody="[20]" --condition_on_primer=true --inject_primer_during_generation=false
+
+#3) get names of things in the not processed folder 
+
 $OFS = "`r`n"
- 
+
 #that sets the output field seperator, meaning when we collect the childitems and add them to the variable, each var has a carriage return and a new line put at the end of them. or start, i dunno and i dont care.
- 
-#$fileNames = Get-ChildItem -Path "DIR_WHERE_GENERATED_FILES_ARE_GOING" -Recurse | select -exp Name
- 
- 
+
+$fileNames = Get-ChildItem -Path "E:\SNES MIDIS\Generated Midi" -Recurse | select -exp fullName
+
+
 #4) feed the names into the batch processing file
-(Get-Content -path test.txt -Raw) -replace 'REPLACE_TEXT', $filenames | Set-Content -Path test.txt
- 
-#5) run the batch processing
- 
-               this should be somewhere on your pc from the tests the other day.
- 
+
+$original = Get-Content -path "C:\Program Files\REAPER (x64)\batch.txt"
+
+(Get-Content -path "C:\Program Files\REAPER (x64)\batch.txt" -Raw) -replace 'REPLACE_TEXT', $filenames | Set-Content -Path "C:\Program Files\REAPER (x64)\batch.txt"
+
+#5) run the batch processing and sleep for $num_outputs seconds
+
+cd "C:\Program Files\REAPER (x64)"
+.\reaper -batchconvert batch.txt
+Start-Sleep -s 20
 #6) move the midi files to a processed folder once rendering has completed
- 
-Get-ChildItem -Path c:/users/nhqadm/desktop/testies -Recurse -File | Move-Item -Destination c:/users/nhqadm/desktop/testing/
- 
+
+Get-ChildItem -Path "E:\SNES MIDIS\Generated Midi" -Recurse -File | Move-Item -Destination "E:\SNES MIDIS\Rendered Midi"
+
+#7 paste original to file
+
+$original | out-file "C:\Program Files\REAPER (x64)\batch.txt"
